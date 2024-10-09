0bdd71ad5e63ec79f2992cec2ed90d67e27a6621
Allysson Almeida <allyssonif@gmail.com>
2024-10-09 10:23:19 -0300

    feat: update sanity client & resolver

--- node/clients/sanity/index.ts
+++ node/clients/sanity/index.ts
@@ -22,23 +22,35 @@ type QueryResult = {
   result: [StoreTemplateCard];
 };
 
+type Settings = {
+  projectId: string;
+  dataset: string;
+  apiVersion: string;
+  authToken: string;
+};
+
 export class SanityClient extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super(`https://mfaylecy.api.sanity.io/v2024-09-25/data/query/test`, context, options);
+    // super(`https://o2jl7jfz.api.sanity.io/v2022-03-07/data/query/dev-2024-05-23`, context, options);
+    super(`https://o2jl7jfz.api.sanity.io`, context, options);
   }
 
-  private getSanityContent(query: string): Promise<QueryResult> {
-    return this.http.get(`?query=${query}`);
+  private getSanityContent(settings: Settings, query: string): Promise<QueryResult> {
+    return this.http.get(`/v${settings.apiVersion}/data/query/${settings.dataset}?query=${query}`, {
+      headers: {
+        Authorization: `Bearer ${settings.authToken}`,
+      },
+    });
   }
 
-  public async storeTemplatedCards(): Promise<[StoreTemplateCard]> {
-    const content = await this.getSanityContent('*[_type=="storeTemplatedCard"]');
+  public async storeTemplatedCards(settings: Settings): Promise<[StoreTemplateCard]> {
+    const content = await this.getSanityContent(settings, '*[_type=="storeTemplatedCard"]');
 
     return content.result;
   }
 
-  public async footer(): Promise<[StoreTemplateCard]> {
-    const content = await this.getSanityContent('*[_type=="footer"]');
+  public async footer(settings: Settings): Promise<[StoreTemplateCard]> {
+    const content = await this.getSanityContent(settings, '*[_type=="footer"]');
 
     return content.result;
   }
--- node/resolvers/footer.ts
+++ node/resolvers/footer.ts
@@ -1,7 +1,9 @@
-export const footer = (_: unknown, __: unknown, ctx: Context) => {
+export const footer = async (_: unknown, __: unknown, ctx: Context) => {
   const {
     clients: { sanity },
   } = ctx;
 
-  return sanity.footer();
+  const settings = await ctx.clients.apps.getAppSettings(`${process.env.VTEX_APP_ID}`);
+
+  return sanity.footer(settings);
 };
